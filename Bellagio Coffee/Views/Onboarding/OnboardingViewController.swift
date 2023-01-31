0f8abc0fd9b299dc195bd94538e32c1b78e7a22b
Manas Duishoev <manasduishoev@gmail.com>
2023-01-31 20:36:48 +0600

    q

--- Bellagio Coffee/Views/Onboarding/OnboardingViewController.swift
+++ Bellagio Coffee/Views/Onboarding/OnboardingViewController.swift
@@ -42,7 +42,7 @@ class OnboardingViewController: UIViewController {
         slides = [
             OnboardingSlide(title: "Delicious Dishes", description: "Experience a variety of amazing dishes from different cultures around the world.", image: #imageLiteral(resourceName: "slide2")),
             OnboardingSlide(title: "World-Class Chefs", description: "Our dishes are prepared by only the best.", image: #imageLiteral(resourceName: "slide1")),
-            OnboardingSlide(title: "Instant World-Wide Delivery", description: "Your orders will be delivered instantly irrespective of your location around the world.", image: #imageLiteral(resourceName: "slide3"))
+            OnboardingSlide(title: "Instant World-Wide Delivery", description: "Your orders will be delivered instantly irrespective of your location around the world...", image: #imageLiteral(resourceName: "slide3"))
         ]
         
 
